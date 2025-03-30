b496b989d6c425ce9f2a7bd628f97b528af53f45
Sergey Kushner <newness.clanks02@icloud.com>
2025-03-30 18:06:05 +0300

    Update

---
+++ app.js
@@ -0,0 +1,64 @@
+async function loadAppDetail() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const appId = urlParams.get("id");
+    
+    const response = await fetch("../../apps.json");
+    const apps = await response.json();
+    const app = apps.find(a => a.id === appId);
+    
+    if (!app) {
+        document.body.innerHTML = "<h2>App not found</h2>";
+        return;
+    }
+
+    updateMetaTags(app);
+    updateUI(app);
+}
+
+function updateMetaTags(app) {
+    document.title = app.title;
+    document.querySelector('meta[name="description"]').setAttribute("content", app.metaDescription);
+    document.querySelector('meta[property="og:title"]').setAttribute("content", app.title);
+    document.querySelector('meta[property="og:description"]').setAttribute("content", app.metaDescription);
+    const appUrl = `https://sergeykushner.github.io/app.html?id=${app.id}`;
+    document.getElementById('meta-og-url').setAttribute('content', appUrl);
+    document.querySelector('meta[name="apple-itunes-app"]').setAttribute("content", `app-id=${app.appStoreId}`);
+    const shareImageUrl = `https://sergeykushner.github.io/assets/apps/${app.id}/share.jpg`;
+    document.getElementById('meta-og-image').setAttribute('content', shareImageUrl);
+}
+
+function updateUI(app) {
+    const iconPath = `assets/apps/${app.id}/app-icon.png`;
+    document.getElementById("app-icon").src = iconPath;
+    document.getElementById("app-title").textContent = app.title;
+    document.getElementById("app-subtitle").textContent = app.subtitle;
+
+    document.getElementById("app-description").innerHTML = app.fullDescription
+    .map(p => p ? (p.startsWith('- ') ? `<li>${p.substring(2)}</li>` : `<p>${p}</p>`) : '<br>')
+    .join('');
+
+
+    document.getElementById("app-store-link").href = `https://itunes.apple.com/us/app/id${app.appStoreId}`;
+    
+    // Контейнер для Product Hunt бейджа
+    const phContainer = document.getElementById("product-hunt-container");
+    
+    // Проверка наличия Product Hunt бейджа
+    if (app.productHunt) {
+        phContainer.innerHTML = app.productHunt;
+        phContainer.style.display = "block";
+    } else {
+        phContainer.style.display = "none";
+    }
+    
+    document.getElementById("screenshot").src = app.screenshot;
+    
+    // Устанавливаем email в футере
+    document.getElementById("email-link").href = `mailto:${app.email}`;
+    
+    // Обновляем ссылку на страницу Privacy Policy
+    document.getElementById("privacy-link").href = `privacy.html?id=${app.id}`;
+}
+
+// Вызов функции загрузки при загрузке страницы
+loadAppDetail();
---
+++ apps.js
@@ -0,0 +1,31 @@
+async function loadApps() {
+    const response = await fetch("apps.json");
+    const apps = await response.json();
+    const container = document.getElementById("app-container");
+
+    apps.forEach(app => {
+        const appDiv = document.createElement("div");
+
+        appDiv.innerHTML = `
+            <a href="app.html?id=${app.id}">
+                <img data-src="assets/apps/${app.id}/app-icon.png" class="lazyload apps-app-icon" alt="${app.title}">
+            </a>
+        `;
+        container.appendChild(appDiv);
+    });
+
+    // Ленивая загрузка изображений
+    const lazyImages = document.querySelectorAll(".lazyload");
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.src = entry.target.dataset.src;
+                entry.target.classList.remove("lazyload");
+            }
+        });
+    });
+
+    lazyImages.forEach(img => observer.observe(img));
+}
+
+loadApps();
